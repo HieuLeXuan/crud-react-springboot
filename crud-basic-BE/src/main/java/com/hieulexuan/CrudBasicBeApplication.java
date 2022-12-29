ec2d80d6bd57e5927ebddfe6b699c16d67523c65
HieuLeXuan <hieulexuan629@gmail.com>
2022-12-29 22:37:39 +0700

    add new project reactjs

---
+++ crud-basic-BE/src/main/java/com/hieulexuan/CrudBasicBeApplication.java
@@ -0,0 +1,13 @@
+package com.hieulexuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBasicBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBasicBeApplication.class, args);
+	}
+
+}
